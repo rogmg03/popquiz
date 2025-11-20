e5eb229c85fb0ef1d89e33538cc3bfc938c4db65
IsaacLopez777 <lopeztenorio58@gmail.com>
2025-11-19 20:20:22 -0600

    MVC

--- PopQuiz.Demo/obj/Debug/net8.0/PopQuiz.Demo.AssemblyInfo.cs
+++ PopQuiz.Demo/obj/Debug/net8.0/PopQuiz.Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PopQuiz.Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386f92007d8fa81a54864aca8524384a57efda33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31626285d4cc7329c1c481d366ec6126e67d700")]
 [assembly: System.Reflection.AssemblyProductAttribute("PopQuiz.Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PopQuiz.Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
