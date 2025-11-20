99ccedef2a447549ed3542535ad541f783e2b392
Robert Quesada Borbon <robertllds07@gmail.com>
2025-11-19 20:49:48 -0600

    Add Movie functionality

--- PopQuiz.Data/Repositories/RepositoryMovie.cs
+++ PopQuiz.Data/Repositories/RepositoryMovie.cs
@@ -1,4 +1,5 @@
-﻿using PAW.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using PAW.Data.Models;
 using PopQuiz.Data.Models;
 
 namespace PAW.Data.Repositories;
@@ -13,10 +14,21 @@ public interface IRepositoryMovie
     Task<bool> UpdateAsync(Movie entity);
     Task<bool> UpdateManyAsync(IEnumerable<Movie> entities);
     Task<bool> ExistsAsync(Movie entity);
+    Task<IEnumerable<Movie>> SearchByTitleAsync(string title);
 }
 public class RepositoryMovie : RepositoryBase<Movie>, IRepositoryMovie
 {
     public RepositoryMovie(HallOfFameContext context) : base(context)
     {
     }
+    public async Task<IEnumerable<Movie>> SearchByTitleAsync(string title)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            return new List<Movie>();
+        
+        return await DbContext.Movies
+            .Where(m => m.Title.Contains(title))
+            .ToListAsync();
+    }
+
 }
