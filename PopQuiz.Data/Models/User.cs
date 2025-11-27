37f0daec0ca8254dda30139085700e9bafd142bc
rogmg03 <rogermora03@gmail.com>
2025-11-26 20:15:27 -0600

    Gitignore update

--- PopQuiz.Data/Models/User.cs
+++ PopQuiz.Data/Models/User.cs
@@ -7,13 +7,15 @@ public partial class User
 {
     public int UserId { get; set; }
 
+    public string Password { get; set; } = null!;
+
     public string Name { get; set; } = null!;
 
     public string LastName { get; set; } = null!;
 
     public string Email { get; set; } = null!;
 
-    public DateOnly DateBirth { get; set; }
+    public DateTime DateBirth { get; set; }
 
     public int RoleId { get; set; }
 
