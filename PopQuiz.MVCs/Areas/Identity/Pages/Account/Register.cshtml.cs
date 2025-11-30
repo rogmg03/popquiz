dfad58a4fed02d61041dfb26df335e7922db4919
JorgeAlfaro29 <jorgealfarog29@hotmail.com>
2025-11-30 12:20:12 -0600

    cambios en roles

--- PopQuiz.MVCs/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PopQuiz.MVCs/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -135,6 +135,15 @@ namespace PopQuiz.MVCs.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    // Asignar rol al usuario
+                    if (!string.IsNullOrEmpty(Input.Role))
+                    {
+                        if (await _roleManager.RoleExistsAsync(Input.Role))
+                        {
+                            await _userManager.AddToRoleAsync(user, Input.Role);
+                        }
+                    }
+
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
